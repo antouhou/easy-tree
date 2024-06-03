ce430c19372f4d715e9b7013c3d09450ae9aa7eb
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2024-06-03 18:09:33 +0700

    feat: implement simple tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "rayon")]
-use rayon::prelude::*;
-#[cfg(feature = "rayon")]
 pub use rayon;
+#[cfg(feature = "rayon")]
+use rayon::prelude::*;
 
 #[derive(Clone)]
 pub struct Node<T> {
@@ -41,9 +41,7 @@ impl<T> Default for Tree<T> {
 
 impl<T> Tree<T> {
     pub fn new() -> Self {
-        Self {
-            nodes: Vec::new(),
-        }
+        Self { nodes: Vec::new() }
     }
 
     pub fn add_node(&mut self, data: T) -> usize {
@@ -152,7 +150,7 @@ impl<T> Tree<T> {
 #[cfg(feature = "rayon")]
 impl<T: Send + Sync> Tree<T> {
     #[cfg(feature = "rayon")]
-    pub fn par_iter(&self) -> impl rayon::iter::ParallelIterator<Item = (usize, &T)> {
+    pub fn par_iter(&self) -> impl ParallelIterator<Item = (usize, &T)> {
         self.nodes
             .par_iter()
             .enumerate()
@@ -160,10 +158,70 @@ impl<T: Send + Sync> Tree<T> {
     }
 
     #[cfg(feature = "rayon")]
-    pub fn par_iter_mut(&mut self) -> impl rayon::iter::ParallelIterator<Item = (usize, &mut T)> {
+    pub fn par_iter_mut(&mut self) -> impl ParallelIterator<Item = (usize, &mut T)> {
         self.nodes
             .par_iter_mut()
             .enumerate()
             .map(|(index, node)| (index, &mut node.data))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tree() {
+        let mut tree = Tree::new();
+        let root = tree.add_node(0);
+        let child1 = tree.add_child(root, 1);
+        let child2 = tree.add_child(root, 2);
+        let child3 = tree.add_child(child1, 3);
+
+        assert_eq!(tree.get(root), Some(&0));
+        assert_eq!(tree.get(child1), Some(&1));
+        assert_eq!(tree.get(child2), Some(&2));
+        assert_eq!(tree.get(child3), Some(&3));
+
+        assert_eq!(tree.parent_index_unchecked(child1), Some(root));
+        assert_eq!(tree.parent_index_unchecked(child2), Some(root));
+        assert_eq!(tree.parent_index_unchecked(child3), Some(child1));
+
+        assert_eq!(tree.children(root), &[child1, child2]);
+        assert_eq!(tree.children(child1), &[child3]);
+        assert_eq!(tree.children(child2), &[]);
+        assert_eq!(tree.children(child3), &[]);
+    }
+
+    #[test]
+    fn test_tree_iter() {
+        let mut tree = Tree::new();
+        let root = tree.add_node(0);
+        let child1 = tree.add_child(root, 1);
+        let child2 = tree.add_child(root, 2);
+        let child3 = tree.add_child(child1, 3);
+
+        let mut iter = tree.iter();
+        assert_eq!(iter.next(), Some((root, &0)));
+        assert_eq!(iter.next(), Some((child1, &1)));
+        assert_eq!(iter.next(), Some((child2, &2)));
+        assert_eq!(iter.next(), Some((child3, &3)));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_tree_iter_mut() {
+        let mut tree = Tree::new();
+        let root = tree.add_node(0);
+        let child1 = tree.add_child(root, 1);
+        let child2 = tree.add_child(root, 2);
+        let child3 = tree.add_child(child1, 3);
+
+        let mut iter = tree.iter_mut();
+        assert_eq!(iter.next(), Some((root, &mut 0)));
+        assert_eq!(iter.next(), Some((child1, &mut 1)));
+        assert_eq!(iter.next(), Some((child2, &mut 2)));
+        assert_eq!(iter.next(), Some((child3, &mut 3)));
+        assert_eq!(iter.next(), None);
     }
 }
